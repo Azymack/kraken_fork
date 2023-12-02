1a92560130a0b2713832e826f0d258f86c2ff9c7
sleek <orillath@outlook.com>
2023-12-03 05:38:39 +0800

    test commit

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'Kraken',
+  title: 'Nextjs Kraken',
+  description: 'Kraken code for your Nextjs Boilerplate with Tailwind CSS',
   locale: 'en',
 };
